18cab791dd00c4dc6d09286f3f6027ff834ff1a2
Phil Fitch <mostreluctantly@gmail.com>
2020-02-19 15:08:08 -0800

    log asynchronous callback behavior

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const http = require('http');
 
+/*
 http.createServer(function(req, res) {
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.write("Hello World");
+  res.end();
+}).listen(8888);”
+*/
+
+function onRequest(req, res) {
+  console.log('Request received.');
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('Hello World');
   res.end();
-}).listen(8888);
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log('Server has started.');
