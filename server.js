3c6cf94693c2d744d8aa454cea15e593d7e8900a
Phil Fitch <mostreluctantly@gmail.com>
2020-02-19 18:12:01 -0800

    request handlers connected

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ http.createServer(function(req, res) {
 }).listen(8888);”
 */
 
-function start(route) {
+function start(route, handle) {
   function onRequest(req, res) {
     const pathname = url.parse(req.url).pathname;
     console.log('Request for ' + pathname + ' received.');
-    route(pathname);
+    route(handle, pathname);
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hello World');
     res.end();
