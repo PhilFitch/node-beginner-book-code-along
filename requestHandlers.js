c284531639d50f0e18e128f630842984017c5a39
Phil Fitch <mostreluctantly@gmail.com>
2020-03-04 15:19:27 -0800

    handler for  show

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,5 @@
-const querystring = require('querystring');
+const querystring = require('querystring'),
+  fs = require('fs');
 
 function start(res) {
   console.log("Request handler 'start' was called.");
@@ -28,5 +29,12 @@ function upload(res, postData) {
   res.end();
 }
 
+function show(res) {
+  console.log('Request handler \'show\' was called.');
+  res.writeHead(200, {'Content-Type': 'image/png'});
+  fs.createReadStream('/tmp/test.png').pipe(res);
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
